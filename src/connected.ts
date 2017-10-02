d0f4d9ff730db4d79b986c9f9949853f52d90379
Bhavya Wadhwa <bhavya.wadhwa@gmail.com>
2017-10-02 19:18:58 +0530

    Basic Structure for project and minor cleanup - removed editor config file

--- src/connected.ts
+++ src/connected.ts
@@ -1,4 +1,10 @@
-// Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
-// import "core-js/fn/array.find"
-// ...
-export default class DummyClass {}
+import PubSub from "./pubSub"
+
+class Connected extends PubSub {
+  constructor() {
+    super()
+    console.log(this)
+  }
+}
+
+new Connected()
---
+++ src/pubSub.ts
@@ -0,0 +1,7 @@
+export default class PubSub {
+  _events = {}
+
+  constructor() {
+    console.log("inside pubsub constructor")
+  }
+}
